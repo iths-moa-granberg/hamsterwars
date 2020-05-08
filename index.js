9236bf3cbc5307cb178498308fffa6abbe6114fb
Moa Granberg <moa.granberg@hotmail.com>
2020-05-08 13:13:11 +0200

    routes setup

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('public'));
 
-// routes
+const assetsRoute = require('./routes/assets');
+const hamstersRoute = require('./routes/hamsters');
+const chartsRoute = require('./routes/charts');
+const gamesRoute = require('./routes/games');
+const statsRoute = require('./routes/stats');
+
+app.use('/assets', assetsRoute);
+app.use('/hamsters', hamstersRoute);
+app.use('/charts', chartsRoute);
+app.use('/games', gamesRoute);
+app.use('/stats', statsRoute);
 
 app.listen(3000, () => console.log('server running at 3000'));
