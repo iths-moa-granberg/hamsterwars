87b9344bed17961da3f64d94f89b9febef3321a6
Moa Granberg <moa.granberg@hotmail.com>
2020-05-11 11:30:34 +0200

    fix init image import filename continuity

--- scripts/importImages.js
+++ scripts/importImages.js
@@ -3,7 +3,7 @@ const { bucket } = require('../api/firebase');
 const imageToFirebaseStorage = async () => {
     try {
         for (let i = 1; i <= 40; i++) {
-            await bucket.upload(`../hamsters/hamster-${i}.jpg`, { destination: `hamsters/hamster-${i}` });
+            await bucket.upload(`../hamsters/hamster-${i}.jpg`, { destination: `hamsters/hamster-${i}.jpg` });
             console.log(`hamsterpic id ${i} added`);
         }
     } catch (err) {
