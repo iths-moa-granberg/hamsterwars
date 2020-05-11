5086f601c736d9983e4880b50a7afdbd0ab71959
Moa Granberg <moa.granberg@hotmail.com>
2020-05-11 09:32:17 +0200

    import images to firebase storage
    Initial func to upload hamsterpics to firebase storage

--- api/firebase.js
+++ api/firebase.js
@@ -1,13 +1,13 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./serviceAccount.json");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://hamsterwars-4ea5d.firebaseio.com"
+    databaseURL: "https://hamsterwars-4ea5d.firebaseio.com",
+    storageBucket: "hamsterwars-4ea5d.appspot.com"
 });
 
-const auth = admin.auth();
 const db = admin.firestore();
+const bucket = admin.storage().bucket();
 
-module.exports = { auth, db };
+module.exports = { db, bucket };
---
+++ scripts/importImages.js
@@ -0,0 +1,14 @@
+const { bucket } = require('../api/firebase');
+
+const imageToFirebaseStorage = async () => {
+    try {
+        for (let i = 1; i <= 40; i++) {
+            await bucket.upload(`../hamsters/hamster-${i}.jpg`, { destination: `hamsters/hamster-${i}` });
+            console.log(`hamsterpic id ${i} added`);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+imageToFirebaseStorage();
