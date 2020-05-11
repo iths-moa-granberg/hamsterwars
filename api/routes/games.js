6808bf1ede4aefb32e94cc81484ad1b05adfabb3
Moa Granberg <moa.granberg@hotmail.com>
2020-05-11 17:04:58 +0200

    games routes

---
+++ api/routes/games.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+const router = new Router();
+const { db } = require('../firebase');
+
+// get all games
+router.get('/', async (req, res) => {
+    try {
+        const snapshots = await db.collection('games').get();
+        const games = [];
+        snapshots.forEach(doc => games.push(doc.data()));
+        res.send(games);
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// add new game 
+router.post('/', async (req, res) => {
+    try {
+        const game = req.body;
+        game['timestamp'] = new Date();
+        game['id'] = new Date().getUTCMilliseconds();
+
+        if (!checkKeys(game)) {
+            throw 'Error: Incorrect keys';
+        }
+
+        await db.collection('games').doc(game.id.toString()).set(game)
+            .then(res.send({ msg: 'Game added' }))
+            .catch(err => { throw err });
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+const checkKeys = obj => {
+    return obj.contestants.length === 2
+        && Boolean(obj.contestants.find(hamster => hamster.id === obj.winner.id));
+}
+
+module.exports = router;
