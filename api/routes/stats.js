28837a00b2f2417bb2cc73b4d76ab5d25f31961a
Moa Granberg <moa.granberg@hotmail.com>
2020-05-14 11:53:06 +0200

    stats/agree route

--- api/routes/stats.js
+++ api/routes/stats.js
@@ -13,4 +13,31 @@ router.get('/total', async (req, res) => {
     }
 });
 
+// get precentage of previous games with same result
+// url eg: /agree/?winnerId=41&loserId=11
+router.get('/agree', async (req, res) => {
+    try {
+        const winnerId = parseInt(req.query.winnerId);
+        const loserId = parseInt(req.query.loserId);
+        let games = [];
+
+        const snapshots = await db.collection('games')
+            .where('winner.id', 'in', [winnerId, loserId])
+            .get();
+        snapshots.forEach(game => games.push(game.data()));
+
+        games = games.filter(game =>
+            !game.contestants.find(hamster =>
+                hamster.id != winnerId && hamster.id != loserId)
+        );
+
+        const numOfWins = games.filter(game => game.winner.id === winnerId).length;
+
+        res.send({ agreeancePercentage: Math.floor(numOfWins / games.length * 100) });
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
 module.exports = router;
