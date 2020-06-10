3287858e01f16ec77c9069a23d475d295d56d209
Moa Granberg <moa.granberg@hotmail.com>
2020-06-10 14:48:01 +0200

    Router setup, styled header & footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Start from './components/Start';
+import Battle from './components/Battle';
+
+const StyledHeader = styled.header`
+  background-color: var(--main-color);
+  height: 3em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    text-decoration: none;
+    margin: 2em;
+    color: var(--white);
+  }
+`;
+
+const StyledFooter = styled.footer`
+  background-color: var(--main-color);
+  color: var(--white);
+  position: absolute;
+  bottom 0;
+  width: 100vw;
+  height: 2.5em;
+`;
 
 function App() {
-  const url = `/api/hamsters/1`;
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    (async () => {      
-      try {
-        let response = await fetch(url);
-        const json = await response.json();        
-        setData(json);
-      } catch (err) {
-        console.log(err);
-      }
-    })();
-  }, [url]);
 
   return (
-    <div>
-      {data && data.favFood}
-    </div>
+    <Router>
+      <StyledHeader>
+        <nav>
+          <Link to="/battle">Battle</Link>
+          <Link to="/stats">Stats</Link>
+          <Link to="/upload">Upload</Link>
+        </nav>
+      </StyledHeader>
+
+      <main>
+        <Switch>
+          <Route path="/battle/:id1/:id2"><Battle /></Route>
+          <Route path="/battle"><Battle /></Route>
+          <Route path="/matchup/:id1/:id2">matchup</Route>
+          <Route path="/stats">stats</Route>
+          <Route path="/upload">upload</Route>
+          <Route path="/"><Start /></Route>
+        </Switch>
+      </main>
+
+      <StyledFooter></StyledFooter>
+    </Router>
   );
 }
 
---
+++ src/components/Start.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Start = () => {
+    return (
+        <div>detta är startsidan med info om vad det är för sida</div>
+    );
+}
+
+export default Start;
