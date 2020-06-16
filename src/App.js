ebd41a259d8e84722c8bc15e6e0ea778a49ae086
Moa Granberg <moa.granberg@hotmail.com>
2020-06-16 12:44:04 +0200

    Header-component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,29 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 import Start from './components/Start';
 import Battle from './components/Battle';
 import Matchup from './components/Matchup';
 import Stats from './components/Stats';
 import Upload from './components/Upload';
-
-const StyledHeader = styled.header`
-  background-color: var(--main-color);
-  height: 3em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  a {
-    text-decoration: none;
-    margin: 2em;
-    color: var(--white);
-  }
-`;
+import Header from './components/Header';
 
 const StyledFooter = styled.footer`
-  background-color: var(--main-color);
+  background-color: var(--primary-color);
   color: var(--white);
   position: absolute;
   bottom 0;
+  right: 0;
   width: 100vw;
-  height: 2.5em;
+  height: 5em;
 `;
 
 function App() {
 
   return (
     <Router>
-      <StyledHeader>
-        <nav>
-          <Link to="/">Start</Link>
-          <Link to="/battle/">Battle</Link>
-          <Link to="/stats/">Stats</Link>
-          <Link to="/upload/">Upload</Link>
-        </nav>
-      </StyledHeader>
-
+      <Header />
       <main>
         <Switch>
           <Route path="/battle/:id1/:id2/"><Battle /></Route>
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
+
+const Header = () => {
+    return (
+        <header className={styles.root}>
+            <nav>
+                <div>
+                    <Link to="/">
+                        <span role="img" aria-label="hamster">🐹</span>
+                    Hamsterwars
+                </Link>
+                </div>
+                <div>
+                    <Link to="/battle/">Battle</Link>
+                    <Link to="/stats/">Stats</Link>
+                    <Link to="/upload/">Upload</Link>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
